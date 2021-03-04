88b907fe2e84ec952b7ca8a351d6bce860829384
Cam <camglowicki@hotmail.com>
2021-03-03 18:40:41 -0800

    set UI form to  on submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ export default function Form({ setInputText, todos, setToDos, inputText }){
   const inputTextHandler = (event) => {
     //console.log(event.target.value);
     setInputText(event.target.value);
-  }
+  };
   
   const submitToDoHandler = (event) => {
     event.preventDefault();
@@ -15,12 +15,13 @@ export default function Form({ setInputText, todos, setToDos, inputText }){
         completed: false,
         id: Math.random() * 1000
       }
-    ])
-  }
+    ]);
+    setInputText("");
+  };
 
   return (
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
+      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
       <button onClick={submitToDoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
